4480efd4beae2cd3cd9fe67b1822eeb8473ff9a1
ahmetbal2000 <ahmet.bal2000@gmail.com>
2017-01-05 23:29:02 +0300

    Kullanıcı tablosu güncellendi

--- E-Ticaret/Models/Kullanici.cs
+++ E-Ticaret/Models/Kullanici.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -13,11 +14,23 @@ namespace E_Ticaret.Models
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
 
-        [StringLength(20), Required]
-        public string KullaniciAdi { get; set; }
+        [StringLength(20, ErrorMessage = "{0} en fazla {1} karakter olabilir."), Required(ErrorMessage = "{0} boş geçilemez."), DisplayName("Kullanıcı Adı")]
+        public string KULLANICIADI { get; set; }
 
-        [StringLength(250), Required]
-        public string Sifre { get; set; }
+        [StringLength(250, ErrorMessage = "{0} en fazla {1} karakter olabilir."), Required(ErrorMessage = "{0} boş geçilemez."), DisplayName("Şifre")]
+        public string SIFRE { get; set; }
+
+        [StringLength(25, ErrorMessage = "{0} en fazla {1} karakter olabilir."), Required(ErrorMessage = "{0} boş geçilemez."), DisplayName("Ad")]
+        public string AD{ get; set; }
+
+        [StringLength(25, ErrorMessage = "{0} en fazla {1} karakter olabilir."), Required(ErrorMessage = "{0} boş geçilemez."), DisplayName("Soyad")]
+        public string SOYAD { get; set; }
+
+        [StringLength(100, ErrorMessage = "{0} en fazla {1} karakter olabilir."), Required(ErrorMessage = "{0} boş geçilemez."), DisplayName("E-posta")]
+        public string EPOSTA { get; set; }
+
+        [StringLength(10, ErrorMessage = "{0} en fazla {1} karakter olabilir."), DisplayName("Cep telefonu")] 
+        public string CEPTELEFONU { get; set; }
 
     }
 }
